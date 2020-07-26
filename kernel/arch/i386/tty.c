773a793e6f0e001ceda6a967bdc7d206ba48c7c1
hyperbeamf <lordofthesquishies@gmail.com>
2020-07-26 17:43:01 -0400

    iso.sh fix

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -41,12 +41,14 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 void terminal_putchar(char c) {
 	unsigned char uc = c;
 
-	if (c == '\n') {
-	  	terminal_row = ++terminal_row;
+	switch(c) {
+	case '\n':
+		++terminal_row;
 		terminal_column = 0;
 		return;
-	} else {
+	default:
 		terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
+		break;
 	}
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,5 +4,5 @@
 
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Welcome to Solix OS\n");
+	printf("Welcome to Solix OS version 0.0.1 pre-alpha\n");
 }
